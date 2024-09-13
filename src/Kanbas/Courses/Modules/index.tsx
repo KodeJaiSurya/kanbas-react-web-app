91121f6d3f7941daf415531edcb2b105bc2f5ed0
KodeJaiSurya <59638236+KodeJaiSurya@users.noreply.github.com>
2024-09-13 14:07:17 -0400

    github repo change + added module buttons

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,10 +1,16 @@
 export default function Modules() {
   return (
     <div>
-      {/* Implement Collapse All button, View Progress button, etc. */}
+      <button>Collapse All</button> <button>View Progress</button>{" "}
+      <select id="publish-options" name="publish-options">
+        <option value="publish-all">Publish All</option>
+      </select>{" "}
+      <button>+Module</button>
       <ul id="wd-modules">
         <li className="wd-module">
-          <div className="wd-title">Week 1</div>
+          <div className="wd-title">
+            Week 1, Lecture 1 - Course Introduction, Syllabus, Agenda
+          </div>
           <ul className="wd-lessons">
             <li className="wd-lesson">
               <span className="wd-title">LEARNING OBJECTIVES</span>
@@ -14,6 +20,17 @@ export default function Modules() {
                   Learn what is Web Development
                 </li>
               </ul>
+              <li className="wd-content-item">READING</li>
+              <ul className="wd-subcontent">
+                <li>Full Stack Developer - Chapter 1 - Introduction</li>
+                <li>Full Stack Developer - Chapter 2 - Creating User</li>
+              </ul>
+              <li className="wd-content-item">SLIDES</li>
+              <ul className="wd-subcontent">
+                <li>Introduction to Web Development</li>
+                <li>Creating an HTTP server with Node.js</li>
+                <li>Creating a React Application</li>
+              </ul>
             </li>
           </ul>
         </li>
@@ -22,6 +39,20 @@ export default function Modules() {
           <ul className="wd-lessons">
             <li className="wd-lesson">
               <span className="wd-title">LEARNING OBJECTIVES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Learn how to create user interfaces with HTML
+                </li>
+                <li className="wd-content-item">
+                  Deploy the assignment to Netlify
+                </li>
+              </ul>
+              <li className="wd-content-item">SLIDES</li>
+              <ul className="wd-subcontent">
+                <li>Introduction to HTML and the DOM</li>
+                <li>Formatting Web content with Headings and</li>
+                <li>Formatting content with Lists and Tables</li>
+              </ul>
             </li>
           </ul>
         </li>
