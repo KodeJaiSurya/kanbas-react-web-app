df7e5eaa44426f1ee3f123c683404d9f8e5026e6
KodeJaiSurya <59638236+KodeJaiSurya@users.noreply.github.com>
2024-12-08 23:42:01 -0500

    Update QuizControlButtons.tsx

--- src/Kanbas/Courses/Quizzes/QuizControlButtons.tsx
+++ src/Kanbas/Courses/Quizzes/QuizControlButtons.tsx
@@ -1,25 +1,86 @@
 import { IoEllipsisVertical } from "react-icons/io5";
-import GreenCheckmark from "../Modules/GreenCheckmark";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useDispatch } from "react-redux";
 import * as quizzesClient from "./client";
 import { updateQuiz, deleteQuiz } from "./reducer";
 import FacultyRestrictedRoute from "../../FacultyRestrictedRoute";
+import { MdDoNotDisturb, MdOutlineEdit } from "react-icons/md";
+import { FaTrash } from "react-icons/fa";
+import GreenCheckmark from "../Modules/GreenCheckmark";
 
 export default function QuizControlButtons({
   quiz,
   deleteQuiz,
 }: {
-  quiz: any[];
+  quiz: any;
   deleteQuiz: (quizId: string) => void;
 }) {
-  const { qid } = useParams();
+  const { cid } = useParams();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const togglePublish = async () => {
+    const updatedQuiz = {
+      ...quiz,
+      published: !quiz.published,
+      course: cid,
+    };
+    await quizzesClient.updateQuiz(updatedQuiz);
+    dispatch(updateQuiz(updatedQuiz));
+  };
+  const renderPublishButtonText = () =>
+    quiz.published ? "Unpublish" : "Publish";
+  const renderPublishStatus = () =>
+    quiz.published ? <GreenCheckmark /> : <MdDoNotDisturb />;
+
   return (
-    <div className="float-end">
-      <button className="dropwdown">
-        <IoEllipsisVertical className="fs-4 float-end" />
-      </button>
+    <div className="d-flex float-end border-0 flex-end">
+      <FacultyRestrictedRoute>
+        <div>{renderPublishStatus()}</div>
+        <div className="dropdown d-inline">
+          <button
+            className="btn btn-white btn-lg"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <IoEllipsisVertical className="fs-4" />
+          </button>
+          <ul className="dropdown-menu">
+            <li>
+              <button
+                className="dropdown-item"
+                onClick={() =>
+                  navigate(`/Kanbas/Courses/${cid}/Quizzes/${quiz.id}`)
+                }
+              >
+                <MdOutlineEdit className="text-primary me-2" />
+                Edit
+              </button>
+            </li>
+            <li>
+              <button
+                className="dropdown-item"
+                data-bs-toggle="modal"
+                data-bs-target={`#wd-delete-${quiz.id}-dialog`}
+                onClick={() => deleteQuiz(quiz.id)}
+              >
+                <FaTrash className="text-danger me-2" />
+                Delete
+              </button>
+            </li>
+            <li>
+              <button className="dropdown-item" onClick={togglePublish}>
+                {quiz.published ? (
+                  <MdDoNotDisturb className="text-danger me-2" />
+                ) : (
+                  <GreenCheckmark />
+                )}
+                {renderPublishButtonText()}
+              </button>
+            </li>
+          </ul>
+        </div>
+      </FacultyRestrictedRoute>
     </div>
   );
 }
